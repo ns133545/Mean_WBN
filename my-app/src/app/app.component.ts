fb00fbfcb9a4b996ce8387925ab512aeaabfa5bf
unknown <ns133545@gmail.com>
2020-05-21 23:47:45 +0530

    Layout Added

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  isRegistered:boolean;
+  constructor()
+  {
+    this.isRegistered=false;
+  }
+}
